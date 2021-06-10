ca416a570bb0feba7846713aa4ecf116f11a0e13
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-06-10 14:14:34 -0600

    commit tree 77c7879e8fdc9d1e4633d9b8c1b8255b838de328

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+const findMatching = (array, name) => {
+    return array.filter(function(el) {
+      return el.toLowerCase().indexOf(name.toLowerCase()) !== -1
+    })
+}
+
+const fuzzyMatch = (array, name) => {
+    return array.filter(function(el) {
+      return el.toLowerCase().indexOf(name.toLowerCase()) === 0
+    })
+} 
+
+// const matchName = (array, name) => {
+//     let matchedName = [];
+//     for (let i=0; i < array.length; i++) {
+//         if (array[i].name === name) {
+//             matchedName.push(array[i]);
+//         }
+//     }
+//     console.log(matchedName);
+// } 
+
+const matchName = (array, name) => {
+    let matchedName = array.filter(function(cb) {
+        return cb.name === name;
+    });
+    return matchedName;
+}
+
+
